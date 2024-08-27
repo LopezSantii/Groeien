a464506c25e189faa8fc454136844e2549160ccf
Santi <santilopez14301@gmail.com>
2024-08-26 21:26:38 -0300

    update

--- src/services/services.js
+++ src/services/services.js
@@ -1,5 +1,5 @@
 export default function PlanesList() {
-  return fetch("./planes.json")
+  return fetch("/planes.json")
     .then((res) => res.json())
     .catch((error) => {
       console.error("Error fetching data:", error);
